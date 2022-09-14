d110dca9a5b454e0c2ac34af82e5d1a68f21fda0
nazaadimoha <nazaadimoha98@gmail.com>
2022-09-14 22:53:52 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import Chat from './components/Chat';
 
 
+
 const style = {
   appContainer: `max-w-[768px] mx-auto text-center`,
   sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`
@@ -18,7 +19,7 @@ function App() {
       <section className={style.sectionContainer}> 
         {/* NAvbar */}
         <Navbar />
-        
+        {user ? <Chat /> : null}
         {/* chat section */}
         <Chat />
         
